493e93c0a6174dd41d6251edf361320bda84e070
Kalki-Sahayak <142188579+Kalki-Sahayak@users.noreply.github.com>
2023-09-21 16:30:10 +0530

    Add files via upload

---
+++ As09_21.09.2023.py
@@ -0,0 +1,28 @@
+## QUESTION 1
+
+# def fact(x):
+#     f =1 
+#     for i in range (1, x+1):
+#         f = f*i
+#     return (f)
+# def fact_rec(x):
+#     f =1
+#     if x==1:
+#         return 1
+#     else:
+#         f = f*x*fact_rec(x-1)
+#         return f
+# b = int(input("PRESS -1 TO EXIT and CONTINUE PRESS ANY NUMBER"))
+# while (b!= -1):
+#     n = int(input ("Enter the number whose factorial is needed: "))
+#     x = int(input("1. Non recursive method\n2. Recursive method\nEnter your choice: "))
+#     if x == 1:
+#         d=fact(n)
+#         print (d)
+#     if x == 2:
+#         e = fact_rec(n)
+#         print (e)
+
+
+## QUESTION 4
+
